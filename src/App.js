e58dab576bae680ba83e119097acd8ba8dca2e5b
cko003 <cko003@bucknell.edu>
2024-05-30 13:05:53 +0100

    add intellisense for tailwind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import logo from "./logo.svg";
-import "./App.css";
-
 function App() {
   return (
-    <div>
-      <h1 className="text-3l font-bold underline">Hello World!</h1>
+    <div className="App">
+      <h1 className="text-3xl text-center font-bold underline">Hello World!</h1>
+      <h2 className=""></h2>
     </div>
   );
 }
