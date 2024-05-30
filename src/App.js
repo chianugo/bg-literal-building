27c83350f7b4e3d2bf70e9d100ddaa57d66ad3c4
cko003 <cko003@bucknell.edu>
2024-05-30 14:59:27 +0100

    import signup, login, account in app js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Header from "./components/Header";
 import Search from "./components/Search";
+import Account from "./components/Account";
+import SignUp from "./components/SignUp";
+import Login from "./components/Login";
+
 function App() {
   return (
     <div className="App">
       <Header />
-      <Search />
+      {/* <Search /> */}
     </div>
   );
 }
